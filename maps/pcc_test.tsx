07e9054e5b4904bd38d8bf32ebd5cf165a36f1ea
Cheng-Yu Pai <mail4charliepai@gmail.com>
2017-07-16 23:12:18 -0700

    Add tile map test

---
+++ maps/pcc_test.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="pcc_test" tilewidth="32" tileheight="48" tilecount="16" columns="4">
+ <image source="../graphics/pccs/pcc_body_4.bmp" trans="2b8585" width="128" height="192"/>
+</tileset>
---
+++ maps/test_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="outside_tileset" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="667" columns="23">
+ <image source="../graphics/tilesets/outside_tileset.png" width="760" height="958"/>
+</tileset>
